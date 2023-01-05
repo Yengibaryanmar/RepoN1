4886105573181da0fe416fd5fd432337856390a9
Marine <marinengibaryan@gmail.com>
2023-01-05 22:27:56 +0400

    homework

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+      int c;
+      int a =2;
+      int b = 3;
+      c = (a * a) + (b * b);
+      System.out.println("c =" + c);
+     //----------------------------//
+        System.out.println((0.5 * a) * (b + c));
+        //------------------------//
+        int G;
+        int i = 5;
+        int j = 4;
+        int k = 8;
+        G = i * (10*10) + (j*10) + k;
+        System.out.println(" G = "+G);
+        System.out.println((G - (j*10)- k)/100);
+        System.out.println((G- (i * 100)- k)/10);
+        System.out.println((G-(i*100)-(j*10)));
+
+    }
+}
